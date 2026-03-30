6acceb13c3dcbe51344e0972aa1a9cae39b98012
ban878 <vnt878@gmail.com>
2026-03-30 23:25:52 +0200

    Create MainActivity.kt for initializing the copy service

---
+++ app/src/main/java/com/example/copyplus/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.copyplus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize the copy service here
+        // Code for initializing the copy service goes here
+    }
+}
